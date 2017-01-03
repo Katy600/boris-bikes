118ec02ccf46ee80a2ab5397b16af7d0094a71d2
Katy McCann <katy.alice.mccann@gmail.com>
2017-01-03 16:38:28 +0000

    Add dock method with one arguement

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,6 +1,12 @@
 require_relative 'bike'
 class DockingStation
+attr_reader :docked_bike
+
   def release_bike
-    Bike.new
+     Bike.new
+  end
+
+  def dock(bike)
+
   end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -5,8 +5,19 @@ describe DockingStation do
   it 'responds to release bike' do
     expect(subject).to respond_to :release_bike
   end
+
   it 'releases working bikes' do
     bike = subject.release_bike
     expect(bike).to be_working
   end
+
+  # it 'docks bikes' do
+  #   docking_station = DockingStation.new
+  #   bike = subject.release_bike
+  #   docking_station.dock(bike)
+  #   expect(subject.docked_bike).to eq :docked_bike
+  # end
+
+  it { is_expected.to respond_to(:dock).with(1).argument }
+
 end
