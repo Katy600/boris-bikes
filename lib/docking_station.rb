7d51220bbb15a79ad456b9b35f4988158ddcc725
Katy McCann <katy.alice.mccann@gmail.com>
2017-01-03 11:20:53 +0000

    failing test for respends to release bike

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,3 +1,3 @@
 
 class DockingStation
-end 
+end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,5 +1,8 @@
+require_relative 'spec_helper'
 require 'docking_station'
 
 describe DockingStation do
-
+  it 'responds to release bike' do
+    expect(subject).to respond_to :release_bike
+  end
 end
