da7494dc48f46862503ce1c8e2c8d293f6999bb2
Katy McCann <katy.alice.mccann@gmail.com>
2017-01-04 12:19:19 +0000

    method dock fails if docking station is full

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -8,6 +8,7 @@ attr_reader :bike
   end
 
   def dock(bike)
+    fail 'Docking station full' if @bike
     @bike = bike
   end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -37,5 +37,12 @@ describe DockingStation do
     it 'raises an error when there are no bikes available' do
       expect { subject.release_bike }.to raise_error 'No bikes available'
     end
+
+    describe '#dock' do
+      it 'raises an error when full' do
+        subject.dock(Bike.new)
+        expect { subject.dock Bike.new }.to raise_error 'Docking station full'
+      end
+    end
   end
 end
