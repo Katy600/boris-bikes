53fd263cc92f3f422d44d5e35f0425feea9e2196
Katy McCann <katy.alice.mccann@gmail.com>
2017-01-04 11:11:46 +0000

    release_bike method has message fail when not bikes are available

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -3,6 +3,7 @@ class DockingStation
 attr_reader :bike
 
   def release_bike
+      fail 'No bikes available' unless @bike
      @bike
   end
 
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -25,4 +25,17 @@ describe DockingStation do
     subject.dock(bike)
     expect(subject.bike).to eq bike
   end
+
+  describe '#release_bike' do
+    it 'releases a bike' do
+      bike = Bike.new
+      subject.dock(bike)
+      # we want to release the bike we docked
+      expect(subject.release_bike).to eq bike
+    end
+
+    it 'raises an error when there are no bikes available' do
+      expect { subject.release_bike }.to raise_error 'No bikes available'
+    end
+  end
 end
