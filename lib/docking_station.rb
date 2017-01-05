28b96bc75d2b98c15ce16f2db4531809f45f9eb9
Katy McCann <katy.alice.mccann@gmail.com>
2017-01-05 11:56:38 +0000

    Create constant DEFAULT_CAPACITY to hold my magic number

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -2,24 +2,26 @@ require_relative 'hired_bike'
 
 class DockingStation
 attr_reader :bikes
+DEFAULT_CAPACITY = 20
+
   def initialize
     @bikes = []
   end
 
   def release_bike
-      raise 'No bikes available' if empty?
+      fail 'No bikes available' if empty?
        @bikes.pop
   end
 
   def dock(bike)
-    raise 'Docking station full' if full?
+    fail 'Docking station full' if full?
      @bikes.length
      @bikes << bike
   end
 
   private
   def full?
-    @bikes.length >= 21
+    @bikes.length >= DEFAULT_CAPACITY
   end
 
   def empty?
