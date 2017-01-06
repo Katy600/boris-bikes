8144bfaceb76fa2ad7133c02e7cee9953533462c
Katy McCann <katy.alice.mccann@gmail.com>
2017-01-06 14:42:59 +0000

    Create doubles to replace bike instances

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -62,8 +62,8 @@ describe DockingStation do
     end
 
     it 'raises an error when a bike is broken' do
-       bike = Bike.new
-       bike.report_broken
+       bike = double(:bike)
+       allow(bike).to receive(:broken).and_return(true)
        subject.dock(bike)
       expect { subject.release_bike }.to raise_error 'No bikes available'
     end
